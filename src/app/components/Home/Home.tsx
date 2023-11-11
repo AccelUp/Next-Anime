4ee5406bdd4443f9f41fd647b4b0c795872145f6
AccelUp <risetyudhaadhi16@gmail.com>
2023-11-11 15:57:30 +0700

    feat: added list to display top anime

--- src/app/components/Home/Home.tsx
+++ src/app/components/Home/Home.tsx
@@ -1,35 +1,60 @@
 "use client";
-
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Image from "next/image";
 
+interface AnimeData {
+  title: string;
+  synopsis: string;
+  images: {
+    jpg: {
+      image_url: string;
+    };
+  };
+}
+
+interface TopAnime {
+  title: string;
+  image_url: string;
+}
+
 const Home = () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<AnimeData[]>([]);
+  const [topAnime, setTopAnime] = useState<TopAnime[]>([]);
 
   useEffect(() => {
     const getItemsData = async () => {
-      const response = await axios.get(
-        "https://api.jikan.moe/v4/seasons/2023/summer"
-      );
-      const { data } = response.data;
-      // console.log(data);
-      setData(data);
+      try {
+        const [latestResponse, topResponse] = await Promise.all([
+          axios.get("https://api.jikan.moe/v4/seasons/2023/fall"),
+          axios.get("https://api.jikan.moe/v4/top/anime"),
+        ]);
+        const latestData = latestResponse.data.data;
+        const topData = topResponse.data.data;
+        setData(latestData);
+        setTopAnime(topData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
     };
 
     getItemsData();
   }, []);
 
   return (
-    <div className="bg-slate-600">
-      <div className="flex flex-wrap justify-start pt-36 px-2 sm:px-16">
+    <div className="flex flex-row bg-slate-600 min-h-screen pt-36 lg:px-20 pb-10 ">
+      <div className="flex flex-wrap md:w-screen ">
+        <div className="flex items-center text-lg font-bold mb-2 rounded-md px-4 mx-2 h-14 w-full border-b border-slate-400 bg-slate-800">
+          <h1>Latest Anime</h1>
+        </div>
+
         {data.map((item, index) => (
-          <div key={index} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2">
-            <div className="rounded-sm h-60">
+          <div key={index} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/5 p-2 ">
+            <div className="relative">
               <img
-                className="w-full h-full object-cover rounded-sm"
-                src={item.images.webp.image_url}
+                src={item.images.jpg.image_url}
                 alt={item.title}
+                className="object-cover w-screen h-40 rounded-t-md"
               />
             </div>
             <div className="rounded-b-md bg-slate-800 w-full h-36 p-4 flex flex-col">
@@ -39,9 +64,29 @@ const Home = () => {
                 </h1>
               </div>
               <div className="overflow-hidden">
-                <p className="text-justify text-xs">{item.synopsis}</p>
+                <p className="text-justify text-xs pt-2 pb-7">
+                  {item.synopsis}
+                </p>
               </div>
             </div>
+          </div>
+        ))}
+      </div>
+      <div className="flex-col w-[30rem] hidden lg:flex">
+        <div className="flex items-center text-lg font-bold rounded-t-md px-4 mx-2 h-14 w-full border-b border-slate-400 bg-slate-800">
+          <h1>Top Anime</h1>
+        </div>
+        {topAnime.map((top, index) => (
+          <div
+            key={index}
+            className="flex items-center text-lg font-bold px-4 mx-2 h-16 w-full border-b border-slate-400 bg-slate-800"
+          >
+            <img
+              className="h-11 w-10 object-cover"
+              src={top.images.jpg.image_url}
+              alt=""
+            />
+            <div className="pl-3 text-base font-light">{top.title}</div>
           </div>
         ))}
       </div>
--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -3,7 +3,7 @@ const Navbar = () => {
     <>
       <nav className="bg-white px-5 py-4 dark:bg-gray-900 absolute w-full z-20 top-0 left-0 border-gray-200 ">
         <div className="flex flex-row text-center gap-2">
-          <div className="p-2 w-1/3 text-2xl font-extrabold">
+          <div className="p-2 w-1/3 text-2xl font-extrabold ">
             <a href="">JOJOANIME</a>
           </div>
           <div className="relative flex flex-row w-full items-center px-7 text-slate-500">
@@ -17,13 +17,13 @@ const Navbar = () => {
             </button>
           </div>
           <div className="p-2 w-1/4 text-xl">
-            <button className="px-5 h-8 rounded-md bg-blue-700 text-base hover:bg-blue-500">
+            <button className="px-5 h-8 rounded-md bg-blue-700 text-base hover:bg-blue-500 md:text-sm sm:text-xs">
               Sign in
             </button>
           </div>
         </div>
         <div className="flex-row text-center px-20 gap-6 pt-4 hidden lg:flex">
-          <div className="w-1/4  rounded-md font-semibold">Latest</div>
+          <div className="w-1/4 rounded-md font-semibold">Latest</div>
           <div className="w-1/4 rounded-md font-semibold">Weekly</div>
           <div className="w-1/4 rounded-md font-semibold">Monthly</div>
           <div className="w-1/4 rounded-md font-semibold">Seasons</div>
